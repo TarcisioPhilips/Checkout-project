5065eb6973c8137f3d0a60a6b0be9891bbfaea51
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-07-23 23:20:25 -0300

    feat: config stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,20 @@
+import { styled } from "@/styles";
+
+const Button = styled("button", {
+  backgroundColor: "$tarcisio",
+  borderRadius: 4,
+  border: 0,
+  padding: "4px 8px",
+  span: {
+    fontWeight: "bold",
+  },
+});
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <Button>
+      <span>Teste</span>
+      Enviar
+    </Button>
+  );
 }
